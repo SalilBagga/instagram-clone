8df7e2a2e844eed1f307ad7ef5904e4cc7b2fd14
Salil Bagga <gaganmeet@hybrowlabs.com>
2022-02-07 15:44:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="bg-insta-back min-h-screen">
+      <Header />
+      <h1>HI</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="bg-white p-5 object-contain border-b-1 border-grey-500">
+      <img
+        className=""
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt=""
+      />
+    </div>
+  );
+}
